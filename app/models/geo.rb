4af76cdc2fb370934bd54979eae4a1c5555741fb
Nikola Mitev <mitev.nkl@gmail.com>
2022-11-16 11:45:14 +0100

    country-flags done

--- app/models/geo.rb
+++ app/models/geo.rb
@@ -2,7 +2,9 @@ class Geo < ApplicationRecord
   belongs_to :post
   # geocoded_by :place
   # after_validation :geocode, if: :will_save_change_to_place?
-  def country_code
-    Geocoder.search(self.place).first.country_code
+  def country
+    # get the country from the place
+    place = self.place.split(',').last
+    place.strip
   end
 end
