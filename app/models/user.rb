4a599270e816d2e3de716e01241b6145f70521d8
Nikola Mitev <mitev.nkl@gmail.com>
2022-12-25 13:47:50 +0100

    sengrid for confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :pickups, through: :posts
   # acts_as_tagger
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,17 @@
 require "active_support/core_ext/integer/time"
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => ENV['SENDGRID_API_KEY'],
+  :domain => 'cargster.co',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://cargster.co" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
