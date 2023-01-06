51f741f35ff56b477ee5213431b7f052dddbfd4d
Nikola Mitev <mitev.nkl@gmail.com>
2023-01-06 20:48:58 +0100

    matchamer

--- app/models/load.rb
+++ app/models/load.rb
@@ -3,10 +3,10 @@ class Load < Post
     active_trucks = Truck.eager_load(:pickup, :dropoff).where(['user_id != ? AND status = ?', self.user_id, true])
     truck_matches = []
     active_trucks.each do |t|
-      if (self.pickup.distance_to(t.pickup) <= 50 ||
+      if (self.pickup.distance_to(t.pickup) < 50 ||
         self.pickup.overlap?(t.pickup) ||
         self.pickup.region == t.pickup.region) &&
-        (self.dropoff.distance_to(t.dropoff) <= 50 ||
+        (self.dropoff.distance_to(t.dropoff) < 50 ||
         self.dropoff.overlap?(t.dropoff) ||
         self.dropoff.region == t.dropoff.region) &&
         [-3,-2,-1,0,1,2,3].include?((t.loading_date - self.loading_date).to_i) &&
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -3,10 +3,10 @@ class Truck < Post
     active_loads = Load.eager_load(:pickup, :dropoff).where(['user_id != ? AND status = ?', self.user_id, true])
     l_matches = []
     active_loads.each do |l|
-      if (self.pickup.distance_to(l.pickup) <= 50 ||
+      if (self.pickup.distance_to(l.pickup) < 50 ||
         self.pickup.overlap?(l.pickup) ||
         self.pickup.region == l.pickup.region) &&
-        (self.dropoff.distance_to(l.dropoff) <= 50 ||
+        (self.dropoff.distance_to(l.dropoff) < 50 ||
         self.dropoff.overlap?(l.dropoff) ||
         self.dropoff.region == l.dropoff.region) &&
         [-3,-2,-1,0,1,2,3].include?((l.loading_date - self.loading_date).to_i) &&
