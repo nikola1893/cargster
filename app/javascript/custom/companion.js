2d069f95f9c6c903217730259bc60b1b73d35b81
Nikola Mitev <mitev.nkl@gmail.com>
2023-02-28 12:39:31 +0100

    bg sync disabled from companion.js

--- app/javascript/custom/companion.js
+++ app/javascript/custom/companion.js
@@ -1,11 +1,23 @@
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register("/service-worker.js", { scope: "/" })
+//   .then(() => navigator.serviceWorker.ready)
+//   .then((registration) => {
+//     if ("SyncManager" in window) {
+//       registration.sync.register("sync-forms");
+//     } else {
+//       window.alert("This browser does not support background sync.")
+//     }
+//   }).then(() => console.log("[Companion]", "Service worker registered!"));
+// }
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/service-worker.js", { scope: "/" })
   .then(() => navigator.serviceWorker.ready)
   .then((registration) => {
     if ("SyncManager" in window) {
       registration.sync.register("sync-forms");
+      console.log("[Companion]", "Background sync registered!");
     } else {
-      window.alert("This browser does not support background sync.")
+      console.log("[Companion]", "Background sync is not supported.");
     }
   }).then(() => console.log("[Companion]", "Service worker registered!"));
 }
