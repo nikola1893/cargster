731fe9b392aecd40cc6edbd55ff657a6a944a963
Nikola Mitev <mitev.nkl@gmail.com>
2023-03-10 14:23:10 +0100

    home page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-      redirect_to trucks_path
+      redirect_to all_loads_path
     else
       @page_name = "Kаргстер"
     end
@@ -10,6 +10,16 @@ class PagesController < ApplicationController
   def profile
     @page_name = "За мене"
     @user = current_user
+  end
+
+  def all_loads
+    @page_name = "Денешни објави"
+    @loads = Load.eager_load(:pickup, :dropoff).where(status: true).where.not(user_id: current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+  end
+
+  def all_trucks
+    @page_name = "Денешни објави"
+    @trucks = Truck.eager_load(:pickup, :dropoff).where(status: true).where.not(user_id: current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 
   def privacy
